f44b38a97d2ba3598a6b03c3d7359362a1cb668a
flakestan65-lang <flakestan65@gmail.com>
2026-05-23 11:26:32 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// ЗАМЕНИ ЭТИ ЗНАЧЕНИЯ НА СВОИ ИЗ FIREBASE CONSOLE!
+const firebaseConfig = {
+    apiKey: "ВАШ_API_KEY",
+    authDomain: "ВАШ_ПРОЕКТ.firebaseapp.com",
+    databaseURL: "https://ВАШ_ПРОЕКТ-default-rtdb.firebaseio.com",
+    projectId: "ВАШ_ПРОЕКТ",
+    storageBucket: "ВАШ_ПРОЕКТ.appspot.com",
+    messagingSenderId: "ВАШ_SENDER_ID",
+    appId: "ВАШ_APP_ID"
+};
+
+// EmailJS конфиг (заполни после регистрации на emailjs.com)
+const EMAILJS_CONFIG = {
+    publicKey: "ВАШ_PUBLIC_KEY",
+    serviceId: "ВАШ_SERVICE_ID",
+    templateId: "ВАШ_TEMPLATE_ID"
+};
+
+// Инициализация
+firebase.initializeApp(firebaseConfig);
+emailjs.init(EMAILJS_CONFIG.publicKey);
