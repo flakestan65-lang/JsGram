d2780bd21b31f8e9b4df44e5296d5bccaeb0d9a8
flakestan65-lang <flakestan65@gmail.com>
2026-05-23 11:45:23 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,17 @@
 const firebaseConfig = {
-    apiKey: "ВАШ_API_KEY",
-    authDomain: "ВАШ_ПРОЕКТ.firebaseapp.com",
-    databaseURL: "https://ВАШ_ПРОЕКТ-default-rtdb.firebaseio.com",
-    projectId: "ВАШ_ПРОЕКТ",
-    storageBucket: "ВАШ_ПРОЕКТ.appspot.com",
-    messagingSenderId: "ВАШ_SENDER_ID",
-    appId: "ВАШ_APP_ID"
+  apiKey: "AIzaSyAfQNDcgOADP3X8atKitFkbUB6bCReJbgc",
+  authDomain: "tipo-e35b0.firebaseapp.com",
+  databaseURL: "https://tipo-e35b0-default-rtdb.firebaseio.com",
+  projectId: "tipo-e35b0",
+  storageBucket: "tipo-e35b0.firebasestorage.app",
+  messagingSenderId: "296054755923",
+  appId: "1:296054755923:web:06d906ca61147c66dc6ca6",
 };
 
 const EMAILJS_CONFIG = {
-    publicKey: "ВАШ_PUBLIC_KEY",
-    serviceId: "ВАШ_SERVICE_ID",
-    templateId: "ВАШ_TEMPLATE_ID"
+    publicKey: "KtLrxT8nK55dICe1X",
+    serviceId: "service_15ma2rk",
+    templateId: "template_c0h6x3m"
 };
 
 firebase.initializeApp(firebaseConfig);
